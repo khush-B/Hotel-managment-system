cf71a3d9604c88524b1d7316b0b8361e2588b3ad
Khush <s233967@dtu.dk>
2025-11-03 16:17:19 +0100

    Initial commit

---
+++ src/main/java/com/hms/hotelmanagmentsystem/HotelManagmentSystemApplication.java
@@ -0,0 +1,13 @@
+package com.hms.hotelmanagmentsystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HotelManagmentSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HotelManagmentSystemApplication.class, args);
+	}
+
+}
